b829767877418dc06ba74db4d9ffd0ecbeb5aa2b
vfz <thefritzvalzado@gmail.com>
2024-02-20 23:31:24 +0800

    added base routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
 
-function App() {
+import React from 'react';
+import { BrowserRouter , Route, Routes } from 'react-router-dom';
+import HOAI from './pages/HOAI';
+import Home from './pages/Home';
+import Layout from './pages/Layout';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes> 
+        <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/HOAI" element={<HOAI />} />
+            <Route path="*" element={<HOAI />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
-}
-
+};
+// 
 export default App;
---
+++ src/pages/HOAI.js
@@ -0,0 +1,5 @@
+const HOAI = () => {
+    return <h1>404</h1>;
+  };
+  
+  export default HOAI;
