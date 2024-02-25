21b764caf13faa2a28064eb1f767e2723cc1c5f4
vfz <thefritzvalzado@gmail.com>
2024-02-26 02:41:07 +0800

    rewrote time diff for accuracy

--- src/pages/HOAI.js
+++ src/pages/HOAI.js
@@ -1,25 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import '../css/HOAI.css'; // Import your CSS file
-import formatElapsedTime from 'date-fns'
+import { differenceInMilliseconds, differenceInMonths } from 'date-fns';
+
 
 
 const TimePassed = () => {
   const [timePassed, setTimePassed] = useState('');
 
   const calculateTimePassed = () => {
-    const startTime = new Date('07-01-1998');
+    const secondsInMs = 1000;
+    const minutesInMs = secondsInMs * 60;
+    const hoursInMs = minutesInMs * 60;
+    const daysInMs = hoursInMs * 24;
+    const monthsInMs = daysInMs * (365.25 / 12);
+    const yearsInMs = daysInMs * 365.25;
+
+    let startTime = new Date('1998-07-01T00:00:00');
     const currentTime = new Date();
-    const difference = Math.abs(currentTime - startTime);
-
-
-    const milisec = Math.floor((difference % 1000));
-    const seconds = Math.floor((difference / 1000) % 60);
-    const minutes = Math.floor((difference / (1000 * 60)) % 60);
-    const hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
-    const days = Math.floor(difference / (1000 * 60 * 60 * 24) % 365.25);
-    const years = Math.floor(difference / (1000 * 60 * 60 * 24 * 365.25));
-
-    setTimePassed(`${years}:${days}:${hours}:${minutes}:${seconds}:${milisec}`);
+    const differenceInMs = differenceInMilliseconds(currentTime, startTime); // Difference in milliseconds
+    let tempdif = differenceInMs;
+    const years = Math.floor(Math.abs(tempdif / yearsInMs));
+    tempdif %= yearsInMs;
+    const months = Math.floor(Math.abs(tempdif / monthsInMs));
+    tempdif %= monthsInMs;
+    const days = Math.floor(Math.abs(tempdif / daysInMs));
+    tempdif %= daysInMs;
+    const hours = Math.floor(Math.abs(tempdif / hoursInMs));
+    tempdif %= hoursInMs;
+    const minutes = Math.floor(Math.abs(tempdif / minutesInMs));
+    tempdif %= minutesInMs;
+    const seconds = Math.floor(Math.abs(tempdif / secondsInMs));
+    const milliseconds = Math.abs(differenceInMs % 1000);
+    
+    const formattedDifference = `
+      ${years.toString()}:${months.toString().padStart(2, '0')}:${days.toString().padStart(2, '0')}:${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}:${milliseconds.toString().padStart(3, '0')}`;
+    
+    setTimePassed(formattedDifference);
+    //setTimePassed(`${years}:${months}:${days}:${hours}:${minutes}:${seconds}:${milisec}`);
   };
 
   useEffect(() => {
@@ -32,10 +49,10 @@ const TimePassed = () => {
 
   
 
+
   return (
     <div>
       <h1 className='header-text'>How_Old_Am_I</h1>
-      
       {timePassed.split('').map((digit, index) => (
         <span key={index} className='nixie-tube-lights'>{digit}</span>
       ))}
