2ac2e3a9b5bca000df2a49d498adb413713f232f
vfz <thefritzvalzado@gmail.com>
2024-03-22 00:01:03 +0800

    start here(cleaned files)

--- src/App.js
+++ src/App.js
@@ -3,13 +3,9 @@ import RadialMenu from './pages/RadialMenu'; // Import the RadialMenu component
 import './App.css';
 
 const App = () => {
-  const handleOptionClick = (option) => {
-    // Do something with the selected option, e.g., navigate to a different page
-    console.log('Selected option:', option);
-  };
   return (
     <div className="App">
-    <RadialMenu onOptionClick={handleOptionClick} />
+      <RadialMenu  />
     </div>
   );
 };
---
+++ src/pages/RadialMenu.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RadialMenu = () => {
+  return (
+    <div className="RadialMenu">
+    <h1>RADIAL</h1>
+    </div>
+  );
+};
+
+export default RadialMenu;
