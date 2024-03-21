e03f6032e7fafad9d5055e4d5d59fcbe9d7b8855
vfz <thefritzvalzado@gmail.com>
2024-03-21 22:11:27 +0800

    structure change + radial menu files added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-// App.js
-
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HOAI from './pages/HOAI';
-import Home from './pages/Home';
-import Layout from './pages/Layout';
+import RadialMenu from './pages/RadialMenu'; // Import the RadialMenu component
+import './App.css';
 
 const App = () => {
+  const handleOptionClick = (option) => {
+    // Do something with the selected option, e.g., navigate to a different page
+    console.log('Selected option:', option);
+  };
   return (
-    <Router basename="/vfpages">
-      <Routes > 
-        <Route  path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="/HOAI" element={<HOAI />} />
-            <Route path="*" element={<HOAI />} />
-        </Route>
-      </Routes>
-    </Router>
+    <div className="App">
+    <RadialMenu onOptionClick={handleOptionClick} />
+    </div>
   );
 };
-// 
+
 export default App;
