2f8317483a59e55be7bce6e8b977189a5f8f29ac
vfz <thefritzvalzado@gmail.com>
2024-04-19 06:29:10 +0800

    remaking home and nav

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 // App.js
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HOAI from './pages/HOAI';
 import Home from './pages/Home';
 import Layout from './pages/Layout';
 
 const App = () => {
-  return (
-    <Router basename="/vfpages">
-      <Routes > 
-        <Route  path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="/HOAI" element={<HOAI />} />
-            <Route path="*" element={<HOAI />} />
-        </Route>
-      </Routes>
-    </Router>
-  );
+  
 };
 // 
 export default App;
